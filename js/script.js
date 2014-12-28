de4694b41416d56745af4719a9f7ad4c3a7178bf
Angira Shirahatti <ashirahattia@berkeley.edu>
2014-12-28 14:20:47 -0800

    JQuery where are you???

---
+++ js/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+
+    $('#intro').animate({left: 50, opacity: 'show'}, 1500);
+
+    $('#navigation').delay(1500).fadeIn(1000);
+
+    $('#landing').waypoint(function(direction) {
+          $('#navigation').css({
+            'background-color': '#CE513E'
+          });
+    });
+
+    $('#work').waypoint(function(direction) {
+          $('#navigation').css({
+            'background-color': '#93C6A2'
+          });
+    }, {offset: '30'});
+
+    $('#play').waypoint(function(direction) {
+          $('#navigation').css({
+            'background-color': '#E9B13D'
+          });
+    }, {offset: '30'});
+
+     $('#landingwaypoint').waypoint(function(direction) {
+          $('#navigation').css({
+            'background-color': '#CE513E',
+            'transition': 'linear 0.3s'
+          });
+    });
+
+    $('#workwaypoint').waypoint(function(direction) {
+          $('#navigation').css({
+            'background-color': '#93C6A2'
+          });
+    });
+});
